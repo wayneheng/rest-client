815a20be803eec7835bb780d98037082784ec044
Weijian Heng <weijianheng@gmail.com>
2011-10-19 17:07:10 +0800

    reroute for 302

--- lib/restclient/abstract_response.rb
+++ lib/restclient/abstract_response.rb
@@ -32,15 +32,18 @@ module RestClient
     # Return the default behavior corresponding to the response code:
     # the response itself for code in 200..206, redirection for 301, 302 and 307 in get and head cases, redirection for 303 and an exception in other cases
     def return! request = nil, result = nil, & block
+      #binding.pry
+      #debugger
       if (200..207).include? code
         self
-      elsif [301, 302, 307].include? code
+      #elsif [301, 302, 307].include? code
+      elsif [301, 307].include? code  #removing the 302 return exception (weijian)
         unless [:get, :head].include? args[:method]
           raise Exceptions::EXCEPTIONS_MAP[code].new(self, code)
         else
           follow_redirection(request, result, & block)
         end
-      elsif code == 303
+      elsif code == 303 || code == 302
         args[:method] = :get
         args.delete :payload
         follow_redirection(request, result, & block)
